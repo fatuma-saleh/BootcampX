1c14ad0ac33964d797196c761a0356a9fcc19a3b
Fatuma <sfatex@gmail.com>
2021-09-30 22:08:44 -0400

    Add more queries involving teachers and assistance_requests

---
+++ 4_queries/12_teachers_that_assisted.sql
@@ -0,0 +1,8 @@
+SELECT teachers.name AS teacher,cohorts.name AS cohort 
+FROM assistance_requests
+JOIN teachers ON assistance_requests.teacher_id = teachers.id
+JOIN students ON assistance_requests.student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name ='JUL02'
+GROUP BY teachers.name,cohorts.name
+ORDER BY teachers.name;
